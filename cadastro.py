45a41f4820b0cb611bbce57522f8c7a7cf1297eb
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-04-28 10:27:28 -0300

    Add files via upload

---
+++ cadastro.py
@@ -0,0 +1,21 @@
+print ("Digite o seu nome:") 
+nome = input()
+
+print ("Digite a sua idade:") 
+idade = input() 
+
+print ("Digite seu sexo (M/F)") 
+sexo = input()
+
+print("Digite o seu bairro") 
+bairro = input() 
+
+print ("Digite a sua cidade")
+cidade = input() 
+
+print("-"*16)
+print("O seu nome é:",nome) 
+print("A sua idade é:", idade) 
+print("Seu sexo é:",sexo)
+print("Seu bairro é:",sexo)
+print("A sua cidade é:",cidade)
