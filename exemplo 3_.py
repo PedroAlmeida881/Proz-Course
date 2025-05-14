d343b034015f662bbb245e25a31cf94e60dfebe5
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-05-14 15:39:23 -0300

    Add files via upload

---
+++ exemplo 3_.py
@@ -0,0 +1,19 @@
+Nome = [0] *8
+
+Nome[0] = 'Davi'
+Nome[1] = 'Pedro'
+Nome[2] = 'Henrique'
+Nome[3] = 'Sandro' 
+Nome[4] = 'Carlos'
+Nome[5] = 'Ana'
+Nome[6] = 'Carolina'
+Nome[7] = 'Jaqueline'
+print('Os nomes dos alunos são:')
+print(Nome[0])
+print(Nome[1])
+print(Nome[2])
+print(Nome[3])
+print(Nome[4])
+print(Nome[5])
+print(Nome[6])
+print(Nome[7])
