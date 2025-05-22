8f03294d43e50b4c00d7d08a1b3cff378b29637d
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-05-22 10:47:12 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,6 @@
+#Cálculo da área de um retângulo:
+print('Para saber o resultado da área do retângulo digite a base e a altura do retângulo:')
+base=float(input('Digite o valor da base do retãngulo:'))
+altura=float(input('Digite o valor da altura do retângulo:'))
+area= base*altura
+print('O valor da altura é de:', area) 
