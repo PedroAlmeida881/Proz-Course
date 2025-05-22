67589431567a6fb392f6ad2bf78c2ae1d6e8ac90
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-05-22 10:47:43 -0300

    Add files via upload

---
+++ exercicio4.py
@@ -0,0 +1,5 @@
+#Cálculo de Imc:
+peso=float(input('Digite seu peso:'))
+altura=float(input('Digite sua altura:'))
+IMC = peso / (altura * altura)
+print(f'Seu índice de Massa Corporal(IMC) é:{IMC:.2f}') 
