5beeedfb58b7b548da4f6d69b809337b4be01928
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-05-26 10:51:58 -0300

    Add files via upload

---
+++ dicionário.py
@@ -0,0 +1,12 @@
+Daniela ={  
+           'Nome': 'Daniela',
+           'sobrenome': 'Souza',
+           'profissão': 'Analisa de Sistemas',
+           'filhos': 'Helena'
+         }
+print(Daniela['Nome'])
+print(Daniela['sobrenome'])
+print(Daniela['profissão'])
+print(Daniela['filhos'])
+print()
+print (len(Daniela))
