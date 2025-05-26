e8ffff2769428b293219c1455eb05615d2e28160
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-05-26 10:27:27 -0300

    Add files via upload

---
+++ tuplas2.py
@@ -0,0 +1,13 @@
+frutas = ('maçã', 'banana', 'cereja')
+print('Minha tupla de frutas', frutas)
+print('Primeira fruta:', frutas[0])
+print('Primeira fruta:', frutas[1])
+print('Primeira fruta:', frutas[2])
+print('Número de frutas na tupla:',len(frutas))
+print('Listando todas as frutas na tupla:')
+for fruta in frutas:
+    print(fruta)
+if 'banana' in frutas:
+    print('Banana esta na tupla!')
+unica_fruta = ('Melancia',)
+print('Tupla de um único elemneto:', unica_fruta) 
