2ab5a6782636922343f635dd9a97222386bb27cc
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-05-30 10:59:35 -0300

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,5 @@
+#Conversor de temperatura de celsius para Fahrenheit:
+print('Digite a tempertura em celsius:')
+Celsius=int(input())
+Fahrenheit =int(Celsius * 9/5) + 32
+print('A temperatura em Fahrenheit é:', Fahrenheit) 
