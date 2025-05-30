46d960dcb523b9f430c1b7091908f86edf79254e
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-05-30 16:46:45 -0300

    Add files via upload

---
+++ exercício13.py
@@ -0,0 +1,8 @@
+# Solicita o número ao usuário
+numero = int(input("Digite um número inteiro: "))
+
+# Verifica se é divisível por 3 e por 5 ao mesmo tempo
+if numero % 3 == 0 and numero % 5 == 0:
+    print("O número é divisível por 3 e por 5 ao mesmo tempo.")
+else:
+    print("O número NÃO é divisível por 3 e por 5 ao mesmo tempo.")
