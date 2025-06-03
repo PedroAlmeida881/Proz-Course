ba8c75a66ee690ad67e22173a405c7fd70fb4194
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-06-03 11:12:54 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,156 +1,46 @@
-#Menu de uma pizzaria
+print('Digite o número correspondente ao mês:')
+print('''1-Janeiro
+2- Fevereiro
+3- Março 
+4- Abril
+5- Maio
+6- Junho  
+7= Julho 
+8- Agosto 
+9- Setembro  
+10- Outubro  
+11-Novembro  
+12- Dezembro ''') 
 
-print("Pizzaria del primo - Seja bem vindo!")
-print("---Cardápio - preços---")
-print(" ")
-print("***** Pizzas - sabores********")
-print(" BOLANHESA, PEPERONI, PORTUGUESA")
-print("*****Pizzas - Sabores*********")
-print(" Pizza pequena R$ 10,00 ")
-print(" Pizza Média R$ 15,00  ") 
-print(" Pizza Grande R$ 20,00  ")
-print("*******Refrigerantes********")
-print(" Coca-cola 500 ML     R$ 7,00  ")
-print(" Guaraná  500 ML     R$ 6,00  ")
-print(" Fanta    500 ML     R$ 5,00  ")
-print("-"*60)
-print(" ")
-print("Faça o seu pedido para pizza:")
-print("1- Bolonhesa")
-print("2- Peperoni")
-print("3- Portuguesa")
-print("-"*60)
+try: 
+    numero= int(input('Sua escolha:'))
 
-pedidopizza= int(input())
+    match numero:
+        case 1: 
+         MES = 'Janeiro'
+        case 2: 
+         MES = 'Fevereiro'
+        case 3: 
+         MES = 'Março'
+        case 4: 
+         MES = 'Abril'
+        case 5: 
+         MES= 'Maio'
+        case 6: 
+         MES= 'Junho'
+        case 7: 
+         MES= 'Julho' 
+        case 8: 
+         MES= 'Agosto'
+        case 9: 
+         MES= 'Setembro'
+        case 10: 
+         MES= 'Outubro'
+        case 11: 
+         MES= 'Novembro'
+        case 12: 
+         MES= 'Dezembro'      
+except ValueError:
+      MES = ('Entrada inválida, digite um  número inteiro')
 
-print("Digite o tamanho da pizza:")
-print("P - pequena")
-print("M - Média")
-print("G - Grande")
-print("-"*60)
-
-tampizza= input().upper()
-
-print("Faça seu pedido para refrigerante:")
-print("1- Coca-cola")
-print("2- Guaraná")
-print("3- Fanta")
-print("-"*60)
-
-pedidorefri = int(input()) 
-
-if (pedidopizza == 1) and (tampizza == "P") and (pedidorefri ==1):
-    precopagar = 10.00 + 7.00 
-    pedidos = "Bolonhesa, Pequena, Coca-cola"
-
-elif (pedidopizza == 1) and (tampizza == "P") and (pedidorefri == 2):
-    precopagar = 10.00 + 6.00 
-    pedidos = "Bolonhesa, Pequena, Guaraná"   
-
-elif (pedidopizza == 1) and (tampizza == "P") and (pedidorefri == 3):
-    precopagar = 10.00 + 5.00 
-    pedidos = "Bolonhesa, Pequena, Fanta" 
-
-elif (pedidopizza == 1) and (tampizza == "M") and (pedidorefri == 1):
-    precopagar = 15.00 + 7.00 
-    pedidos = "Bolonhesa, Média, Coca-cola" 
-
-elif (pedidopizza == 1) and (tampizza == "M") and (pedidorefri == 2):
-    precopagar = 15.00 + 6.00 
-    pedidos = "Bolonhesa, Média, Guaraná" 
-
-elif (pedidopizza == 1) and (tampizza == "M") and (pedidorefri == 3):
-    precopagar = 15.00 + 5.00
-    pedidos = "Bolonhesa, Média, Fanta" 
-
-elif (pedidopizza == 1) and (tampizza == "G") and (pedidorefri == 1):
-    precopagar = 20.00 + 7.00 
-    pedidos = "Bolonhesa, Grande, Coca-cola"
-
-elif (pedidopizza == 1) and (tampizza == "G") and (pedidorefri == 2):
-    precopagar = 20.00 + 6.00 
-    pedidos = "Bolonhesa, Grande, Guaraná "
-
-elif (pedidopizza == 1) and (tampizza == "G") and (pedidorefri == 3):
-    precopagar = 20.00 + 5.00 
-    pedidos = "Bolonhesa, Grande, Fanta"
-
-elif (pedidopizza == 2) and (tampizza == "P") and (pedidorefri == 1):
-    precopagar = 10.00 + 7.00 
-    pedidos = "Peperoni, Pequena, Coca-cola"
-
-elif (pedidopizza == 3) and (tampizza == "P") and (pedidorefri == 1):
-    precopagar = 10.00 + 7.00 
-    pedidos = "Portuguesa, Pequena, Coca-cola"
-
-elif (pedidopizza == 2) and (tampizza == "M") and (pedidorefri == 1):
-    precopagar = 15.00 + 7.00 
-    pedidos = "Peperoni, Média, Coca-cola"
-
-elif (pedidopizza == 3) and (tampizza == "M") and (pedidorefri == 1):
-    precopagar = 15.00 + 7.00 
-    pedidos = "Portuguesa, Média, Coca-cola" 
-
-elif (pedidopizza == 2) and (tampizza == "G") and (pedidorefri == 1):
-    precopagar = 20.00 + 7.00 
-    pedidos = "Peperoni, Grande, Coca-cola"
-
-elif (pedidopizza == 3) and (tampizza == "G") and (pedidorefri == 1):
-    precopagar = 20.00 + 7.00 
-    pedidos = "Portuguesa, Grande, Coca-cola" 
-
-elif (pedidopizza == 1) and (tampizza == "P") and (pedidorefri == 2):
-    precopagar = 10.00 + 6.00 
-    pedidos = "Bolonhesa, Pequena, Guaraná"
-
-elif (pedidopizza == 2) and (tampizza == "P") and (pedidorefri == 2):
-    precopagar = 10.00 + 6.00 
-    pedidos = "Peperoni, Pequena, Guaraná"
-
-elif (pedidopizza == 3) and (tampizza == "P") and (pedidorefri == 2):
-    precopagar = 10.00 + 6.00 
-    pedidos = "Portuguesa, Pequena, Guaraná"
-
-elif (pedidopizza == 1) and (tampizza == "M") and (pedidorefri == 2):
-    precopagar = 15.00 + 6.00 
-    pedidos = "Bolonhesa, Média, Guaraná" 
-
-elif (pedidopizza ==2) and (tampizza == "M") and (pedidorefri == 2):
-    precopagar = 15.00 + 6.00 
-    pedidos = "Peperoni, Média, Guaraná"
-
-elif (pedidopizza == 3) and (tampizza == "M") and (pedidorefri == 2):
-    precopagar = 15.00 + 6.00 
-    pedidos = "Portuguesa, Média, Guaraná" 
-
-elif (pedidopizza == 1) and (tampizza == "G") and (pedidorefri == 2):
-    precopagar = 20.00 + 6.00 
-    pedidos = "Bolonhesa, Grande, Guaraná" 
-
-elif (pedidopizza == 2) and (tampizza == "G") and (pedidorefri == 2):
-    precopagar = 20.00 + 6.00 
-    pedidos = "Peperoni, Grande, Guaraná" 
-
-elif (pedidopizza == 3) and (tampizza == "G") and (pedidorefri == 2):
-    precopagar = 20.00 + 6.00 
-    pedidos = "Portuguesa, Grande, Guaraná" 
-
-elif (pedidopizza == 2) and (tampizza == "M") and (pedidorefri == 3):
-    precopagar = 15.00 + 5.00 
-    pedidos = "Peperoni, Média, Fanta" 
-
-elif (pedidopizza == 2) and (tampizza == "P") and (pedidorefri == 3):
-    precopagar = 10.00 + 5.00 
-    pedidos = "Peperoni, Pequena, Fanta" 
-
-elif (pedidopizza == 3) and (tampizza == "M") and (pedidorefri == 3):
-    precopagar = 15.00 + 5.00 
-    pedidos = "Portuguesa, Média, Fanta" 
-
-elif (pedidopizza == 3) and (tampizza == "P") and (pedidorefri == 3):
-    precopagar = 10.00 + 5.00 
-    pedidos = "Portuguesa, Pequena, Fanta" 
-else:
-    print("Pedido inválido. Verifique os dados digitados.")
-    exit()
-print(f"O preço foi R$ {precopagar:.2f} e seu pedido foi: {pedidos}")
+print('O mês selecionado é:', MES)
