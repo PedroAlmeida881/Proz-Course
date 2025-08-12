6d7bbebcc647ee9bb1a52c573bf4bf3f3aaab735
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-08-12 11:11:54 -0300

    Update desafio.js

--- AULAS/desafio.js
+++ AULAS/desafio.js
@@ -1,13 +1,9 @@
 function mudarMensagem() {
     const men= document.getElementById("mensagem");
     men.innerHTML = "Você acabou de GANHAR 5% de descontos em todos os produtos do site! <br> Aproveite!";
-   
-   
-   
-   
-   
     const title= document.getElementById("Titulo");
     title.innerHTML = "Parabèns"
     const button= document.getElementById("botão");
     button.innerHTML = "Obrigrado por clicar"
 }  
+
