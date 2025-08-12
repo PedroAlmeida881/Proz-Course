9c1e4573d6548472258267e298cdea15d949ec99
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-08-12 11:10:41 -0300

    Add files via upload

---
+++ AULAS/desafio.js
@@ -0,0 +1,13 @@
+function mudarMensagem() {
+    const men= document.getElementById("mensagem");
+    men.innerHTML = "Você acabou de GANHAR 5% de descontos em todos os produtos do site! <br> Aproveite!";
+   
+   
+   
+   
+   
+    const title= document.getElementById("Titulo");
+    title.innerHTML = "Parabèns"
+    const button= document.getElementById("botão");
+    button.innerHTML = "Obrigrado por clicar"
+}  
