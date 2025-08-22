9a8a4502359bd9245190346daeba8d67680d6744
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-08-22 10:45:30 -0300

    Add files via upload

---
+++ AULAS/Teste.js
@@ -0,0 +1,32 @@
+function verifica() {
+    let nomes = ["Pedro", "Lucas", "Ana"];
+    let senhas = ["123", "1234", "12345"];
+
+    const mensagem = document.getElementById("msg");
+    const name = document.getElementById("username").value;
+    const acesso = document.getElementById("senha").value;
+
+    let correto = false;
+
+    for (let i = 0; i < nomes.length; i++) {
+        if (name === nomes[i] && acesso === senhas[i]) {
+            correto = true;
+            break;
+        }
+    }
+
+    if (correto) {
+        mensagem.innerHTML = "Login Realizado com Sucesso!";
+    } else {
+        mensagem.innerHTML = "Digitação errada. Verificar Login e Senha!";
+    }
+
+    // Limpar os campos automaticamente
+    document.getElementById("username").value = "";
+    document.getElementById("senha").value = "";
+
+    // Restaurar a mensagem original após 3 segundos
+    setTimeout(() => {
+        mensagem.innerHTML = "Será que você está cadastrado?";
+    }, 3000);
+}
